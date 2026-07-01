e78362309a564abb438dba0f4b3ebe3430ddae23
Serif2121 <136285210+Serif2121@users.noreply.github.com>
2026-07-01 04:18:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ANA SAYFA
 app.get("/", (req, res) => {
   res.send("NEXORA API RUNNING 🚀");
+});
+
+// FEED
+app.get("/feed", (req, res) => {
+  res.json([
+    { id: 1, text: "NEXORA çalışıyor 🚀" },
+    { id: 2, text: "İlk post geldi" },
+    { id: 3, text: "Backend + Frontend bağlı 🔥" }
+  ]);
 });
 
 const PORT = process.env.PORT || 3000;
